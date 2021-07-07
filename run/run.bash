04a5f7a56e1f702478c04d632249d5706152fd5e
517030910405 <lijiasen0921@126.com>
2021-07-07 22:07:49 +0800

    .

--- run/run.bash
+++ run/run.bash
@@ -2,9 +2,9 @@ rm mnist_cnn*
 rm mytime.txt
 python readtime.py > mytime.txt
 bash runs.bash 0&
-bash runs.bash 0&
-bash runs.bash 0&
-bash runs.bash 0
+bash runs.bash 1&
+bash runs.bash 2& 
+bash runs.bash 3
 
 python ensemble.py --nameFrom=0 --nameTo=4
 
